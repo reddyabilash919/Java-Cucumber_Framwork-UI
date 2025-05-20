9600e5a1c2a08a26fd0f6316b0113976bc97ea82
=Abilash Reddy <=areddy@planittesting.com>
2025-05-20 13:48:31 +1000

    updated config file

--- src/main/java/com/automationpractice/utils/Config.java
+++ src/main/java/com/automationpractice/utils/Config.java
@@ -3,16 +3,15 @@ package com.automationpractice.utils;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public abstract class Config {
-    
-    private static final Dotenv dotenv = Dotenv.load();
-    
-    public static final String URL = dotenv.get("URL")!=null? dotenv.get("URL"):System.getenv("URL");
 
-    public static final String BROWSER = dotenv.get("BROWSER")!= null ? dotenv.get("BROWSER") : System.getenv("BROWSER");
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-    public static final String EMAIL = dotenv.get("EMAIL")!= null ? dotenv.get("EMAIL") : System.getenv("EMAIL");
+    public static final String URL = dotenv.get("URL") != null ? dotenv.get("URL") : System.getenv("URL");
 
-    public static final String PASSWORD = dotenv.get("PASSWORD")!= null ? dotenv.get("PASSWORD") : System.getenv("PASSWORD");
+    public static final String BROWSER = dotenv.get("BROWSER") != null ? dotenv.get("BROWSER") : System.getenv("BROWSER");
 
-   
+    public static final String EMAIL = dotenv.get("EMAIL") != null ? dotenv.get("EMAIL") : System.getenv("EMAIL");
+
+    public static final String PASSWORD = dotenv.get("PASSWORD") != null ? dotenv.get("PASSWORD") : System.getenv("PASSWORD");
+
 }
