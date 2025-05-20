8febd60c95329fbe6e274df810f334496f96952a
=Abilash Reddy <=areddy@planittesting.com>
2025-05-20 11:56:22 +1000

    updated loggin info

--- src/test/java/com/automationpractice/hooks/Hooks.java
+++ src/test/java/com/automationpractice/hooks/Hooks.java
@@ -9,12 +9,15 @@ import io.cucumber.java.Before;
 public class Hooks {
 
     /**
-     * This method is executed before each scenario.
-     * It initializes the WebDriver based on the browser specified in the .env file.
+     * This method is executed before each scenario. It initializes the
+     * WebDriver based on the browser specified in the .env file.
      */
     @Before
     public void setup() {
-       
+        System.err.println("Browser: " + Config.BROWSER);
+        System.err.println("URL: " + Config.URL);
+        System.err.println("Email: " + Config.EMAIL);
+        System.err.println("Password: " + Config.PASSWORD);
         DriverManager.initDriver(Config.BROWSER);
 
     }
